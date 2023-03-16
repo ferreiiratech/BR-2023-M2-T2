25ff1a3db8cae4cd45f0830704cf29d99193479a
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2023-03-16 19:37:12 -0300

    AULA 1: Player Dinosaur

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,62 @@
+import pygame
+from dino_runner.utils.constants import *
+
+
+X_POS = 80
+Y_POS = 310
+JUMP_VEL = 8.5
+
+class Dinosaur:
+    def __init__(self):
+        self.image = RUNNING[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = X_POS
+        self.dino_rect.y = Y_POS
+        self.step_index = 0
+        self.jump_vel = JUMP_VEL
+        self.dino_jump = False
+        self.dino_run = True
+
+    def update(self, user_imput):
+        if self.dino_run:
+            self.run()
+
+        if self.dino_jump:
+            self.jump()
+
+        if user_imput[pygame.K_UP] and not self.dino_jump:
+            self.dino_jump = True
+            self.dino_run = False
+        elif not self.dino_jump:
+            self.dino_jump = False
+            self.dino_run = True
+
+        if self.step_index >= 10:
+            self.step_index = 0
+
+    def run(self):
+        self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = X_POS
+        self.dino_rect.y = Y_POS
+        self.step_index += 1
+
+    def jump(self):
+        self.image = JUMPING
+        if self.dino_jump:
+            self.dino_rect.y -= self.jump_vel * 4
+            self.jump_vel -= 0.8
+
+        if self.jump_vel < -JUMP_VEL:
+            self.dino_rect.y = Y_POS
+            self.dino_jump = False
+            self.jump_vel = JUMP_VEL
+
+    def duck(self):
+        #quando receber a tecla para baixo ele vai abaixar
+        pass
+    
+    def draw(self, screen):
+        screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
+
+        
--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -2,6 +2,8 @@ import pygame
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 
+from dino_runner.components.dinosaur import Dinosaur
+
 
 class Game:
     def __init__(self):
@@ -14,6 +16,8 @@ class Game:
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
+
+        self.player = Dinosaur()
 
     def run(self):
         # Game loop: events - update - draw
@@ -30,12 +34,14 @@ class Game:
                 self.playing = False
 
     def update(self):
-        pass
+        user_imput = pygame.key.get_pressed()
+        self.player.update(user_imput)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.player.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
